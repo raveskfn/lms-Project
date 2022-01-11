10037883936046f9fa7ceb0504fd59dc6c3fb007
JEONG <raveskfn@naver.com>
2022-01-12 00:55:15 +0900

    add project

---
+++ src/main/java/com/example/demo/MainPage.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainPage {
+	
+	@RequestMapping("/")
+	public String index() {
+		
+		return "Main Page";
+	}
+
+
+}
